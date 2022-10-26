eeeb22cbbb3a0c7ea86a78a2abb2fbb329c356a1
Rodion Pozniakov <rad@Rodions-MacBook-Pro.local>
2022-10-26 15:07:23 +0300

    added build folder

---
+++ build/index.esm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+var TestComponent = function (_a) {
+    _a.theme;
+    return (React.createElement("div", null,
+        React.createElement("h1", { className: "heading" }, "I'm the test component")));
+};
+
+export { TestComponent };
+//# sourceMappingURL=index.esm.js.map
---
+++ build/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var React = require('react');
+
+var TestComponent = function (_a) {
+    _a.theme;
+    return (React.createElement("div", null,
+        React.createElement("h1", { className: "heading" }, "I'm the test component")));
+};
+
+exports.TestComponent = TestComponent;
+//# sourceMappingURL=index.js.map
